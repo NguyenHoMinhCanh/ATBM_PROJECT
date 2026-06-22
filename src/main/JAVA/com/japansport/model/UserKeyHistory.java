a452bbf0d1717999e99dd850ffb26240f6ac3d26
minh.canh <20130209@st.hcmuaf.edu.vn>
2026-06-22 13:32:12 +0700

    update lịch sử thay đổi khóa

---
+++ src/main/JAVA/com/japansport/model/UserKeyHistory.java
@@ -0,0 +1,54 @@
+package com.japansport.model;
+
+import java.sql.Timestamp;
+
+public class UserKeyHistory {
+    private int id;
+    private int userId;
+    private String publicKey;
+    private Timestamp createdAt;
+    private Timestamp expiredAt;
+
+    public UserKeyHistory() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getExpiredAt() {
+        return expiredAt;
+    }
+
+    public void setExpiredAt(Timestamp expiredAt) {
+        this.expiredAt = expiredAt;
+    }
+}
